a4bfbcdb3d8d71b71651d202e37bc821e11791de
infastin <infastin@yandex.com>
2023-10-21 23:00:05 +0500

    feat: allow urls in subject

--- lib/widgets/subject.dart
+++ lib/widgets/subject.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../entity/entity.dart';
 
@@ -19,6 +21,15 @@ class SubjectWidget extends StatelessWidget {
     var textTheme = theme.textTheme;
     var colorScheme = theme.colorScheme;
     var loc = AppLocalizations.of(context)!;
+
+    var room = subject.room;
+    Uri? uri;
+
+    if (room.startsWith("Discord;")) {
+      final parts = room.split(";");
+      uri = Uri.tryParse(parts.sublist(1).join());
+      room = parts[0];
+    }
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
@@ -46,8 +57,18 @@ class SubjectWidget extends StatelessWidget {
               width: 12,
               child: _dot,
             ),
-            Text(
-              subject.room,
+            if (uri != null) RichText(
+              text: TextSpan(
+                text: room,
+                style: textTheme.labelMedium?.copyWith(
+                  color: colorScheme.primary.withOpacity(0.7),
+                ),
+                recognizer: TapGestureRecognizer()..onTap = () {
+                  launchUrl(uri!);
+                }
+              )
+            ) else Text(
+              room,
               style: textTheme.labelMedium?.copyWith(
                 color: colorScheme.onSurface.withOpacity(0.7),
               ),
